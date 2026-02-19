cc8ceabfc496f86bca44a5664d5c90013f33d64d
saima365 <saimaislam365@gmail.com>
2026-02-19 12:08:30 +0600

    accessor, mutation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -48,4 +50,19 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+        //accessor (getter method)
+    public function getNameAttribute($value){
+        return ucfirst($value);
+    }
+    public function Name():Attribute{
+        return Attribute::make(
+            get:fn($value)=>ucfirst($value)
+        );
+    }
+
+//Mutator (getter method)
+
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,5 +27,6 @@ Route::get("/role",[RoleController::class,"index"]);
 
 Route::apiResource("customers",CustomerController::class);
 Route::apiResource("transactions",TransactionController::class);
+Route::apiResource("users",UserController::class);
 Route::get("transaction_types",[TransactionController::class,'transaction_types']);
 Route::get("account_number/{id}",[TransactionController::class,'account_number']);
